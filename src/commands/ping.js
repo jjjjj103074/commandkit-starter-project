543d86ee01bab9697572bad66b3ecddccd19c2fc
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2024-07-05 16:07:05 +0800

    需改架構成commandkit

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,11 +2,23 @@
 const data = {
   name: 'ping',
   description: 'Pong!',
+  options: [
+    {
+      name: 'user',
+      description: 'User to ping',
+      type: 'USER',
+      required: true,
+    }
+  ],
 };
 
 /** @param {import('commandkit').SlashCommandProps} param0 */
-function run({ interaction, client }) {
-  interaction.reply(`:ping_pong: Pong! ${client.ws.ping}ms`);
+async function run({ interaction, client }) {
+  const embed = new EmbedBuilder()
+  .setAuthor()
+  .setColor('#0099ff')
+  .setTitle('title')
+await interaction.reply({embeds:[embed]});
 }
 
 /** @type {import('commandkit').CommandOptions} */
